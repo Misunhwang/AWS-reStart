edb8b3bd1590d2ec4ca408651d0400b092cd0cff
Sunny Hwang <anabelly1004@gmail.com>
2023-11-11 22:12:06 +1300

    homework6 modified

--- homework/triangle-area.py
+++ homework/triangle-area.py
@@ -11,14 +11,9 @@ class Triangle(Shape):
         self.b = b
         self.c = c
 
-        try:
-            assert a+b > c
-            assert b+c > a
-            assert c+a > b
-        except Exception as e:
-            print(f"-- Error happen : {e}")
-        else:
-            print("-- Valid")
+        assert a+b > c
+        assert b+c > a # AssertionError for Triangle(5,2,1)
+        assert c+a > b
 
     def area(self):
         semi = (self.a + self.b + self.c)/2
@@ -37,25 +32,22 @@ print("\nTriangle(6,3,7)")
 trangle = Triangle(6,3,7)
 print("Area  : ", trangle.area())
 
-print("\nTriangle(1,2,5)")
-trangle = Triangle(1,2,5)
+print("\nTriangle(5,2,1)")
+trangle = Triangle(5,2,1) # AssertionError 
 print("Area : ", trangle.area())
+
 
 """
 <Expect results>
 
 Triangle(3,4,5)
--- Valid
 Area :  6.0
 
 Triangle(8,9,5)
--- Valid
 Area  :  19.9
 
 Triangle(6,3,7)
--- Valid
 Area  :  8.94
 
-Triangle(1,2,5)
--- Error happen : 
+Triangle(5,2,1)
 """
