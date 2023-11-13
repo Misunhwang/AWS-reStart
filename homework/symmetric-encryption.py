e30c5279393003b019022bda7c6d00f1779690a1
Sunny Hwang <anabelly1004@gmail.com>
2023-11-14 10:02:00 +1300

    encrypt homework

---
+++ homework/symmetric-encryption.py
@@ -0,0 +1,10 @@
+from base64 import b64encode, b64decode
+import hashlib
+from Cryptodome.Cipher import AES
+import os
+from Cryptodome.Random import get_random_bytes
+
+def encrypt():
+    print("")
+
+str = "AWSreStart"
