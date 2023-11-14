2d2f092db74b410029dd9c2059ed70ffe94125bd
Sunny Hwang <anabelly1004@gmail.com>
2023-11-14 23:18:23 +1300

    updated

--- homework/week3_hw2/main.py
+++ homework/week3_hw2/main.py
@@ -3,6 +3,14 @@ from pydantic import BaseModel, field_validator
 class BookItem(BaseModel):
     name : str
     author : str
+    year_published : int
+
+    @field_validator("year_published")
+    @classmethod
+    def check_valid_country(cls, year_published: int):
+        assert year_published >=2023, "year_published must be greater than 2023"
+        assert year_published < 3000, "year_published must be less than 3000"
+        return year_published
 
 class ItemOrigin(BaseModel):
     country: str
