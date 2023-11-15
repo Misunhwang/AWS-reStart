b34129ecd024af4acc0da96c19f96f12780c13b9
Sunny Hwang <anabelly1004@gmail.com>
2023-11-16 07:59:29 +1300

    week3 day3 class in the middle

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from typing import Union
+from pydantic import BaseModel
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message" : "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id" : item_id, "q":q}
+
+class Item(BaseModel):
+    name:str
+    price:float
+
+#upgrades
+@app.put('/items/{item_id}')
+def update_item(item_id:int, item:Item):
+    return {"item_id" : item_id, "item":item}
