11d225d86073d650865dadcc32755d1b827c7b70
Sunny Hwang <anabelly1004@gmail.com>
2023-11-16 16:58:45 +1300

    Add Week3 Homework3

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -9,9 +9,12 @@ async def root():
     return {"message" : "Hello World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None): # default the value of 'q' to 'None'
     return {"item_id" : item_id, "q":q}
 
+#asd = read_item(2, "something")
+
+"""
 class Item(BaseModel):
     name:str
     price:float
@@ -20,3 +23,19 @@ class Item(BaseModel):
 @app.put('/items/{item_id}')
 def update_item(item_id:int, item:Item):
     return {"item_id" : item_id, "item":item}
+
+"""
+
+"""
+<Theory>
+my_str:str
+my_str_list: List 
+my_str_list: List[st] = ["str1", "str2"]
+Dict[str, int] = {
+    'k':'v',
+    'k':'v'
+     }
+    ==> 'k' : str
+    =>  'v' : None, str, int (can be)
+Dict [str, Union[None, str, int]]
+"""
