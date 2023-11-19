5d3a622104d7568a31dbb6db56652fd2baec4e16
Sunny Hwang <anabelly1004@gmail.com>
2023-11-20 11:20:51 +1300

    week4 day 1 class 1

--- Inclass/week3_hw1/test/test_main.py
+++ Inclass/week3_hw1/test/test_main.py
@@ -1,15 +1,16 @@
 # pytest
 
 from fastapi.testclient import TestClient
-#from fastapi import FastAPI, HTTPException
+
+# from fastapi import FastAPI, HTTPException
 
 from main import app
 
 
 # Look for file names with test_*.py
-#def test_*():
+# def test_*():
 #    pass
-#test_*.py
+# test_*.py
 
 # unit tests
 """
@@ -17,22 +18,26 @@ install 'pytest' : pip install pytest
 > python -m pytest
 > python -m pytest -vvl
 """
+
+
 def test_basic_example():
     # pass
-    assert(True)
+    assert True
+
 
 client = TestClient(app)
 
+
 def test_put_api():
-    response = client.put("/items/10ok", json = {
-        "name": "book2",
-        "quantity": 5,
-        "serial_num": "10ok",
-        "origin": {
-            "country": "Ethiopia",
-            "production_date": "13/11/2023"        
-        }
-    })
+    response = client.put(
+        "/items/10ok",
+        json={
+            "name": "book2",
+            "quantity": 5,
+            "serial_num": "10ok",
+            "origin": {"country": "Ethiopia", "production_date": "13/11/2023"},
+        },
+    )
 
     print(response.json())
     assert response.status_code == 200
---
+++ Inclass/week3_hw4_withH1/test/testing_fixture.py
@@ -0,0 +1,47 @@
+from fastapi.testclient import TestClient
+
+from main import app
+import pytest
+
+client = TestClient(app)
+
+# to use GET/DELETE API, PUT api needs to get used first - code repetition
+# 1) fixture - precondition to test case. Pre-initialization of an object and return it
+# 2) Providing a matrix of inputs and receiving a matrix of outputs
+# 3) Mock - blackbox testing
+
+
+@pytest.fixture
+def good_payload():
+    return {
+        "name": "book1",
+        "quantity": 3,
+        "serial_num": "10ok",
+        "origin": {"country": "Ethiopia", "production_date": "13/11/2023"},
+    }
+
+
+@pytest.fixture
+def bad_payload():
+    return {
+        "name": "book2",
+        "quantity": 5,
+        "serial_num": "20ok",
+        "origin": {"country": "Ethiopia", "production_date": "13/11/2023"},
+    }
+
+
+def test_put_api_422(bad_payload):
+    print("\n----- Test PUT API with incorrect format -----\n")
+    response = client.put("/items/20ok", json=bad_payload)
+
+    assert response.status_code == 422
+
+
+@pytest.fixture
+def create_and_delete_item(good_payload):
+    client.put("/items/20ok", json=good_payload)
+    yield "Item created"
+    client.delete(f"/items/20ok/{good_payload.serial_num}")
+    
+def test_get_item()
