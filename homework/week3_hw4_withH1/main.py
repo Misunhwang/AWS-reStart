6f946b3121502bbf62fa97a8d3a713a30d4247fe
Sunny Hwang <anabelly1004@gmail.com>
2023-11-29 18:24:24 +1300

    .

--- homework/week3_hw4_withH1/main.py
+++ homework/week3_hw4_withH1/main.py
@@ -9,32 +9,35 @@ app = FastAPI()
 # 2) Define my_inventory_item_dict Dictionary
 my_inventory_item_dict: Dict[str, InventoryItem] = {}
 
+
 # 3) Create 'PUT' APIs
 @app.put("/items/{serial_num}")
-def create_item(item: InventoryItem, serial_num: str) -> None :
+def create_item(item: InventoryItem, serial_num: str) -> None:
     my_inventory_item_dict[serial_num] = item
     print(my_inventory_item_dict)
 
+
 # 4) Create 'GET' APIs, Handle exception 404
 @app.get("/items/{serial_num}")
-def get_item(serial_num: str) -> InventoryItem :
+def get_item(serial_num: str) -> InventoryItem:
     if serial_num in my_inventory_item_dict.keys():
         return my_inventory_item_dict[serial_num]
     else:
         raise HTTPException(status_code=404, detail="Item not found : " + serial_num)
- 
+
+
 # 5) Create 'Delete' APIs
 @app.delete("/items/{serial_num}")
-def delete_item(serial_num: str) -> Dict :
+def delete_item(serial_num: str) -> Dict:
     if serial_num in my_inventory_item_dict.keys():
         my_inventory_item_dict.pop(serial_num)
         print(my_inventory_item_dict)
-        return {"msg":"Successfully deleted"}
+        return {"msg": "Successfully deleted"}
     else:
         raise HTTPException(status_code=404, detail="Item not found : " + serial_num)
 
-# 6) Create 'GET' APIs (all items saved)   
+
+# 6) Create 'GET' APIs (all items saved)
 @app.get("/items/")
 def get_items() -> List[InventoryItem]:
     return my_inventory_item_dict.values()
-
